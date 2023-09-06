f0e5db56b0c1617b02ed5fb9894a7ceadf8bb576
admin <danielpuscasjr99@gmail.com>
2023-09-06 14:37:21 +0100

    Add project files.

---
+++ ShoppingKart/Data/ItemsDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingKart.Data
+{
+    public struct ItemsDictionary
+    {
+        public Dictionary<string, double> GetPriceList() 
+        {
+
+            Dictionary<string, double> Items =  new Dictionary<string, double>()
+            {
+                {"A",5.00},
+                {"B",3.00},
+                {"C", 2.00},
+                {"D",1.50}
+            };
+
+         return Items;
+
+        }
+    }
+}
+
---
+++ ShoppingKart/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ShoppingKart.Data;
+using System.Globalization;
+
+using IHost host = CreateHostBuilder(args).Build();
+
+using var scope = host.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+
+IHostBuilder CreateHostBuilder(string[] strings)
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+        {
+           
+        });
+}
+
+
+string userInput = string.Empty;
+string[] checkoutList;
+
+Console.WriteLine("Item  Price  Offer");
+Console.WriteLine("--------------------");
+
+ItemsDictionary items = new ItemsDictionary();
+Dictionary<string, double> priceList = items.GetPriceList();
+
+foreach (var key in priceList.Keys)
+{
+    Console.WriteLine(key + "     " + priceList[key]);
+}
+
+Console.ReadKey();
